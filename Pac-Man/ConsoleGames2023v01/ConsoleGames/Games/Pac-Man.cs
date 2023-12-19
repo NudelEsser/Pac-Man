4e8d4b20bdbfaeb6b97412139841de9310cb36c0
unknown <luca_ostirgmx.ch>
2023-12-19 12:17:22 +0100

    Progress of last lesson. Score is now displayed on the screen and increases when the yellow square is touched. Yellow square now teleports to a random position on the map.

--- Pac-Man/ConsoleGames2023v01/ConsoleGames/Games/Pac-Man.cs
+++ Pac-Man/ConsoleGames2023v01/ConsoleGames/Games/Pac-Man.cs
@@ -14,33 +14,37 @@ namespace ConsoleGames.Games
         public override string Credits => "Luca Osti, lucaosti@ksr.ch";
         public override int Year => 2023;
         public override bool TheHigherTheBetter => true;
-        public override int LevelMax => 400;
+        public override int LevelMax => 8;
         public override Score HighScore { get; set; }
-
         public override Score Play(int level = 1)
         {
+
             int[] player = new int[2] { 50, 4 };
             int[] oldplayer = new int[2] { 0, 0 };
-            int[] prey = new int[2] { 1, 1};
-            List<int[]> scores = new List<int[]>();
-            List<int[]> path = new List<int[]>();
-            int timer = 60000;
-            Display(player, oldplayer);
+            int[] oldprey = new int[2] { 0, 0 };
+            int[] prey = new int[2] { 100, 10};
+            int score = 0;
+            List<int[]> hunters = new List<int[]> 
+            { 
+                new int[] { 20, 20 },
+            };
+            int timer = 1000000;
+            Display(player, oldplayer, prey, oldprey, hunters, score);
             GenerateMap();
             while (timer > 0)
             {
                 oldplayer[0] = player[0];
                 oldplayer[1] = player[1];
                 MoveHunter();
-                Display(player, oldplayer);
-                CheckCollision();
+                Display(player, oldplayer, prey, oldprey, hunters, score);
+                score = CheckCollision(player, prey, oldprey, score);
                 MovePrey();
-                Display(player, oldplayer);
-                CheckCollision();
+                Display(player, oldplayer, prey, oldprey, hunters, score);
+                score = CheckCollision(player, prey, oldprey, score);
                 CheckPlayerImput(player);
-                MovePlayer(player);
-                Display(player, oldplayer);
-                CheckCollision();
+                MovePlayer(player, oldplayer);
+                Display(player, oldplayer, prey, oldprey, hunters, score);
+                score = CheckCollision(player, prey, oldprey, score);
                 timer--;
                 if (timer == 0)
                 {
@@ -72,7 +76,7 @@ namespace ConsoleGames.Games
                 player[0] = player[0] + 2;
             }
         }
-        private void MovePlayer(int[] player)
+        private void MovePlayer(int[] player, int[] oldplayer)
         {
             if (player[0] >= 116)
             {
@@ -90,7 +94,6 @@ namespace ConsoleGames.Games
             {
                 player[1] = player[1] + 1;
             }
-            Console.SetCursorPosition(player[0], player[1]);
         }
         private void MoveHunter()
         {
@@ -100,21 +103,52 @@ namespace ConsoleGames.Games
         {
 
         }
-        private void CheckCollision()
+        private int CheckCollision(int[] player, int[] prey, int[] oldprey, int score)
         {
+            for (int i = -1; i < 2; i++)
+            {
 
+                if (prey[0] + i == player[0])
+                {
+                    if (prey[1] == player[1])
+                    {
+                        oldprey[1] = prey[1];
+                        oldprey[0] = prey[0];
+                        Random rand = new Random();
+                        prey[1] = rand.Next(2, 27);
+                        prey[0] = rand.Next(8, 114);
+
+                        score++;
+                        return score;
+                    }
+                }
+            }
+            return score;
         }
-        private void Display(int[] player, int[] oldplayer)
+        private void Display(int[] player, int[] oldplayer, int[] prey, int[] oldprey, List<int[]> hunters, int score)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(0, 0);
+            Console.Write("  Score: " + score);
             Console.SetCursorPosition(oldplayer[0], oldplayer[1]);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Write("OO");
+            Console.SetCursorPosition(oldprey[0], oldprey[1]);
+            Console.Write("OO");
             Console.SetCursorPosition(player[0], player[1]);
             Console.BackgroundColor = ConsoleColor.Red;
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("PP");
-
+            Console.Write("Pl");
+            Console.SetCursorPosition(prey[0], prey[1]);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Pr");
+            for (int i = 0; i < hunters.Count; i++)
+            {
+                Console.SetCursorPosition(1, prey[1]);
+            }
         }
         private void GenerateMap()
         {
